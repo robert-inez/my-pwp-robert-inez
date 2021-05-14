8fc69191856edc30c28bde97e078ac78f7d213f4
Robert Inez <rinez@cnm.edu>
2021-05-13 21:14:21 -0600

    made major styling changes to site

--- public_html/documentation/script.js
+++
@@ -1,5 +0,0 @@
-const btn = document.querySelector(".dark");
-
-btn.addEventListener("click", function () {
-	document.body.classList.toggle("dark-theme");
-});
---
+++ public_html/script.js
@@ -0,0 +1,5 @@
+const btn = document.querySelector(".dark");
+
+btn.addEventListener("click", function () {
+	document.body.classList.toggle("dark-theme");
+});
